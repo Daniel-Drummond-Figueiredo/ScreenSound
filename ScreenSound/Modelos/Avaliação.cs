88ccc86c1a955019b3b793b477b0717d759f7bc0
Daniel Drummond <ddf.drummond4@gmail.com>
2025-02-02 23:00:41 -0300

    Adicionado Menus

--- ScreenSound/Modelos/Avaliação.cs
+++ ScreenSound/Modelos/Avaliação.cs
@@ -4,6 +4,8 @@ internal class Avaliação
 {
     public Avaliação(int nota)
     {
+        if (nota <= 0) nota = 0;
+        if (nota >= 10) nota = 10;
         Nota = nota;
     }
     public int Nota { get; }
